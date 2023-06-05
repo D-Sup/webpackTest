442ce66de09dc6c1437a0469ca90b739e9b29a47
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2023-06-05 14:23:17 +0900

    git connect

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import { plus } from "./plus.js";
+import './style.css'
+
+// console.log(plus(2, 4));
+// DOMContentLoaded - DOM 파싱이 완료된 직후 
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.innerHTML = `<img src="${rabbit}>`;
+})
